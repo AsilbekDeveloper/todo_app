a99ed595ca7b4bdacb7fbe843e3900712481f1cf
Asilbek Ilhomov <ilhomovasilbek7@gmail.com>
2025-08-22 20:46:28 +0500

    feat: add AppColors to core

---
+++ lib/core/app_colors.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+class AppColors {
+  static const primary = Color(0xFF9395D3);
+  static const primaryLight = Color(0xFFB3B7EE);
+  static const subText = Color(0xFF000000);
+  static const secondary = Color(0xFFFFFFFF);
+  static const unSelected = Color(0xFF8B8787);
+}
