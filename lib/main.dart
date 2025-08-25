120043b742d6f7ab51451787c2481d58dfbfe0a9
Asilbek Ilhomov <ilhomovasilbek7@gmail.com>
2025-08-25 12:20:57 +0500

    Register TaskModelAdapter

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:todo_app/app.dart';
 import 'package:provider/provider.dart';
 import 'package:todo_app/controller/task_controller.dart';
+import 'package:todo_app/model/task_model.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+  if (!Hive.isAdapterRegistered(0)) {
+    Hive.registerAdapter(TaskModelAdapter());
+  }
   runApp(
     MultiProvider(
       providers: [ChangeNotifierProvider(create: (_) => TaskController())],
