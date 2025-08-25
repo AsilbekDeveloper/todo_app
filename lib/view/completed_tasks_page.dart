1a78a04b32ed09c1fa39164fa39e5e2f2b36eac4
Asilbek Ilhomov <ilhomovasilbek7@gmail.com>
2025-08-25 12:10:48 +0500

    Fix language detection to Dart

---
+++ lib/view/completed_tasks_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CompletedTasksPage extends StatelessWidget {
+  const CompletedTasksPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
