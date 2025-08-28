045f6b15441af909231dc1d5f1323a028a80d6b9
Asilbek Ilhomov <ilhomovasilbek7@gmail.com>
2025-08-28 19:12:16 +0500

    Update TaskItem widget implementation

---
+++ lib/view/widget/task_item.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:todo_app/core/app_colors.dart';
+import 'package:todo_app/model/task_model.dart';
+
+class TaskItem extends StatelessWidget {
+  final TaskModel task;
+  final VoidCallback onEdit;
+  final VoidCallback onDelete;
+  final VoidCallback onToggle;
+
+  const TaskItem({
+    super.key,
+    required this.task,
+    required this.onEdit,
+    required this.onDelete,
+    required this.onToggle,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.only(bottom: 21.h),
+      elevation: 4.w,
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 19.w, vertical: 10.h),
+        child: ListTile(
+          contentPadding: EdgeInsets.zero,
+          title: Text(
+            task.title,
+            style: TextStyle(
+              fontSize: 15.sp,
+              fontWeight: FontWeight.w600,
+              decoration: task.isDone ? TextDecoration.lineThrough : null,
+            ),
+          ),
+          subtitle: Text(
+            task.subTitle,
+            style: TextStyle(
+              fontSize: 12.sp,
+              fontWeight: FontWeight.w400,
+              decoration: task.isDone ? TextDecoration.lineThrough : null,
+            ),
+          ),
+          trailing: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              IconButton(
+                onPressed: onEdit,
+                icon: const Icon(
+                  Icons.edit_outlined,
+                  color: AppColors.primaryLight,
+                ),
+              ),
+              IconButton(
+                onPressed: onDelete,
+                icon: const Icon(
+                  CupertinoIcons.delete,
+                  color: AppColors.primaryLight,
+                ),
+              ),
+              IconButton(
+                onPressed: onToggle,
+                icon: const Icon(
+                  CupertinoIcons.check_mark_circled,
+                  color: AppColors.primaryLight,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
